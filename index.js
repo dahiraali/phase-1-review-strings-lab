44ecbc1afa7a0a8cc880ed73d96e8f02a078302b
dahiraali <dahiraali454@gmail.com>
2024-06-10 17:20:50 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+
 // Write your code in this file!
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Grace Hopper';".length;
+//=> 35
+
+currentUser;
+//=> Uncaught ReferenceError: currentUser is not defined
+
+"High " + "five!";
+//=> "High five!"
+
+"We" + ' ' + `can` + " " + 'concat' + `enate` + " as many strings " + 'as our heart ' + `desires.`;
+//=> "We can concatenate as many strings as our heart desires."
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
